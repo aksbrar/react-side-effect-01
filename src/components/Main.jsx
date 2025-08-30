1347e5f69cdf0664576f25966b65e3b6aa1a31ea
aksbrar <aksbrar34@gmail.com>
2025-08-30 12:33:54 -0400

    state, instead of hard coding

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,29 +1,33 @@
-export default function Main() {
-    return (
-        <main>
-            <div className="form">
-                <label>Top Text
-                    <input
-                        type="text"
-                        placeholder="One does not simply"
-                        name="topText"
-                    />
-                </label>
+// imports
+import React from "react";
 
-                <label>Bottom Text
-                    <input
-                        type="text"
-                        placeholder="Walk into Mordor"
-                        name="bottomText"
-                    />
-                </label>
-                <button>Get a new meme image 🖼</button>
-            </div>
-            <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
-            </div>
-        </main>
-    )
+export default function Main() {
+
+  const [memeData, setMemeData] = React.useState({
+    topText : "One does not simply",
+    bottomText : "Walk into Mordor",
+    imgUrl : "http://i.imgflip.com/1bij.jpg"
+  })
+
+  return (
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input type="text" placeholder="One does not simply" name="topText" />
+        </label>
+
+        <label>
+          Bottom Text
+          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
+        </label>
+        <button>Get a new meme image 🖼</button>
+      </div>
+      <div className="meme">
+        <img src={memeData.imgUrl} />
+        <span className="top">{memeData.topText}</span>
+        <span className="bottom">{memeData.bottomText}</span>
+      </div>
+    </main>
+  );
 }
