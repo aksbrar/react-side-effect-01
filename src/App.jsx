092f51b58ae1db03701d868a0faccac819082ff3
aksbrar <aksbrar34@gmail.com>
2025-08-30 12:29:38 -0400

    change in paths

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import Main from "./components/Main"
+import "./style.css"
+
+export default function App() {
+    
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="https://pngimg.com/uploads/trollface/trollface_PNG15.png" 
+            />
+            <h1>Meme Generator</h1>
+        </header>
+    )
+}
